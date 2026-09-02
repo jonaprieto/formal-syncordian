68b75f6cf79ee9f8b360afa87ce77a3a96cb99a5
Jonathan Cubides <prieto.jona@gmail.com>
2026-09-02 12:43:34 -0500

    require documents to have unique line ids and bottom and top sentinels

--- Syncordian/Document.lean
+++ Syncordian/Document.lean
@@ -2,12 +2,13 @@ import Syncordian.Line
 
 namespace Syncordian
 
-structure Document
-  (Position
-   Content
-   Peer
-   : Type)
-  where
+structure Document (Position Content Peer : Type)
+    [spec : PositionSpec Position] where
   lines : List (Line Position Content Peer)
+  unique_ids : (lines.map (fun line => line.fixed.id)).Nodup
+  has_bottom : ∃ line, line ∈ lines ∧
+    line.fixed.id = LineId.bottom ∧ line.fixed.position = spec.bottom
+  has_top : ∃ line, line ∈ lines ∧
+    line.fixed.id = LineId.top ∧ line.fixed.position = spec.top
 
 end Syncordian
